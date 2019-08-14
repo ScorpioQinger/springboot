27630fc8d49c098afa174a8c5a6205b467283945
zhaoqing <zhaoqing@pangpangpig.com>
2019-08-14 17:23:33 +0800

    4-1-2,新建模块

---
+++ chapter4-1-2/src/main/java/com/example/scorpiospace/Chapter412Application.java
@@ -0,0 +1,13 @@
+package com.example.scorpiospace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter412Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter412Application.class, args);
+    }
+
+}
